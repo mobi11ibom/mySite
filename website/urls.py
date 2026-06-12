068b6fea8d2b538011e3e6ca58f0fdc2da6d33d4
MobinRafizade <mobi.ibom1122@gmail.com>
2026-06-12 15:07:06 +0330

    change
    +url

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from website.views import *
 
 urlpatterns = [
+    path('', home_view),       # برای آدرس اصلی: http://127.0.0.1:8000/
    path('home' , home_view),
    path('contact',contact_view),
    path('about',about_view)
